926f9c3616297da5aa15a67d8ac690c858951993
Kent Nagata <ngtknt@me.com>
2017-06-21 13:54:03 +0900

    Add "Use signle-level contexts"

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -28,3 +28,67 @@ RSpec.describe Book do
     end
   end
 end
+
+# -----------------------------------------------------------------------------
+# Use signle-level contexts
+# Not good:
+
+RSpec.describe Book do
+  describe '#reserve' do
+    context 'when already reserved' do
+      let(:reserved) { true }
+      context 'when the users reserversion limit is exceeded' do
+        let(:exceeded) { true }
+        # ...
+      end
+
+      context 'when the users reserversion limit is not exceeded' do
+        let(:exceeded) { false }
+        # ...
+      end
+    end
+
+    context 'the book is available' do
+      let(:reserved) { false }
+      context 'when the users reserversion limit is exceeded' do
+        let(:exceeded) { true }
+        # ...
+      end
+
+      context 'when the users reserversion limit is not exceeded' do
+        let(:exceeded) { false }
+        # ...
+      end
+    end
+  end
+end
+
+# Good:
+
+RSpec.describe Book do
+  describe '#reserve' do
+    context 'when already reserved and the user reserversion limit is exceeded' do
+      let(:reserved) { true }
+      let(:exceeded) { true }
+      # ...
+    end
+
+    context 'when already reserved and the users reserversion limit is not exceeded' do
+      let(:reserved) { true }
+      let(:exceeded) { false }
+      # ...
+    end
+
+    context 'the book is available and the user reserversion limit is exceeded' do
+      let(:reserved) { false }
+      let(:exceeded) { true }
+      # ...
+    end
+
+    context 'the book is available and the user has enough reserversion limit' do
+      let(:reserved) { false }
+      let(:exceeded) { false }
+      # ...
+    end
+  end
+end
