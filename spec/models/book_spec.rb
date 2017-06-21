fdea0018be9379662a60c3f4e4946356e0071a3d
Kent Nagata <ngtknt@me.com>
2017-06-21 12:34:14 +0900

    Add not good case

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,28 @@
+# Describe and context
+# Not good:
+RSpec.describe Book do
+  describe '#reserve' do
+    it 'raises alredy reserved error when already reserved' do
+      # ...
+    end
+
+    it 'reserves the book when the book is available' do
+      # ...
+    end
+  end
+end
+
+# Good:
 RSpec.describe Book do
   describe '#reserve' do
     context 'when already reserved' do
       it 'raises alredy reserved error' do
+        # ...
+      end
+    end
+
+    context 'the book is available' do
+      it 'reserves the book' do
         # ...
       end
     end
